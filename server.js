f62d3288151ec64a7f4a09374d6b172f7aaee626
felipesba <felipesba@gmail.com>
2018-08-30 23:26:56 -0300

    Bcrypt e email
    alteracao para validar o mail e inclusao do modulo bcrypt

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
 
 const	database = {
 	users: [
@@ -40,10 +44,10 @@ app.get('/', (req, res) => {
 
 //EndPoint = signin 
 app.post('/signin', (req,res) => {
-	if (req.body.name === database.users[0].name &&
+	if (req.body.email === database.users[0].email &&
 		req.body.password === database.users[0].password) 
 	{
-		res.json('SUCESS>>>>>>.......');
+		res.json('SUCESS>>>>>>');
 	} else {
 		res.status(400).json('FAILED...');
 	}
